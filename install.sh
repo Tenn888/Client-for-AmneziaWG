a2ddff3f73ae3c24addba4840692ef906cb417c7
Tenn888 <vladislavvasilev2009@gmail.com>
2026-04-28 15:15:11 +0300

    1

--- install.sh
+++ install.sh
@@ -8,28 +8,14 @@ set -e
 if ! command -v yay &> /dev/null
 then
     echo "[!] yay не найден."
-
-    read -p "Установить yay? (y/n): " choice
-    if [[ "$choice" != "y" ]]; then
-        echo "Установка прервана."
-        exit 1
-    fi
-
-    echo "[*] Устанавливаем зависимости..."
-    sudo pacman -S --needed git base-devel
-
-    echo "[*] Клонируем yay..."
-    if [ -d "yay" ]; then
-        echo "[*] Репозиторий yay уже существует"
-    else
-        git clone https://aur.archlinux.org/yay.git
-    fi
-
-    cd yay
-
-    echo "[*] Сборка yay..."
-    makepkg -si
-
-    cd ..
-    rm -rf yay
+    abort
 fi
+
+
+echo "[*] Обновление базы пакетов..."
+yay -Sy
+
+echo "[*] Установка amneziawg..."
+yay -S amneziawg-dkms amneziawg-tools
+
+echo "[+] Готово!"
